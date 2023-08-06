cee12187f25e5cc4a080533d61bd1db777961a6e
veronicarodrigues <ra.veronica14@gmail.com>
2023-08-06 14:52:59 +0800

    Added links to email and phone number

--- src/Contacto.js
+++ src/Contacto.js
@@ -8,20 +8,24 @@ const Contacto = () => {
       <h4>O si lo prefiere, contacte con nosotros</h4>
       <ul className="contacto">
         <li className="phone">
-          <img
-            src="https://img.icons8.com/ios-glyphs/30/iphone.png"
-            alt="iphone"
-            className="phone-icon"
-          />
-          +34 606 981 333
+          <a href="tel:+34606981333">
+            <img
+              src="https://img.icons8.com/ios-glyphs/30/iphone.png"
+              alt="iphone"
+              className="phone-icon"
+            />
+            +34 606 981 333
+          </a>
         </li>
         <li className="email">
-          <img
-            src="https://img.icons8.com/ios/50/new-post--v1.png"
-            alt="new-post--v1"
-            className="email-icon"
-          />
-          natalia.ardanaz@gmail.com
+          <a href="mailto:natalia.ardanaz@gmail.com">
+            <img
+              src="https://img.icons8.com/ios/50/new-post--v1.png"
+              alt="new-post--v1"
+              className="email-icon"
+            />
+            natalia.ardanaz@gmail.com
+          </a>
         </li>
       </ul>
     </div>
